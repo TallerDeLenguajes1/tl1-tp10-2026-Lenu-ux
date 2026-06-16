f4d94f652dde4bedf1f936a25af6b85a33257680
Lenis <lenis99lg@gmail.com>
2026-06-16 13:45:41 -0300

    fin del ejercicio1

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using EspacioTarea;
+//usar API con HttpClient
+
+List <Tarea> tareas;
+tareas=await TareaServices.GetTareas();
+
+foreach (var item in tareas)
+{
+    Console.WriteLine($"Usuario id:{item.UserId}\n Id:{item.Id}\nTitulo:{item.Titulo}\nEstado:{item.Estado}\n");
+}
+
+
+TareaServices.generarArchivo(tareas);
