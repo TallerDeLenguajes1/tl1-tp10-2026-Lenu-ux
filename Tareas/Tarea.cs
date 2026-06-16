904a279b659eb439240915a7d71580d8933651cc
Lenis <lenis99lg@gmail.com>
2026-06-16 12:46:42 -0300

    termine la clase Tarea

--- Tareas/Tarea.cs
+++ Tareas/Tarea.cs
@@ -1,4 +1,6 @@
+using System.Text.Json.Serialization;
 namespace EspacioTarea;
+
 public class Tarea
 {
     private int userId;
@@ -6,8 +8,12 @@ public class Tarea
     private string? titulo;
     private bool estado;
 
+    [JsonPropertyName("userId")]
     public int UserId { get => userId; set => userId = value; }
+    [JsonPropertyName("id")]
     public int Id { get => id; set => id = value; }
+    [JsonPropertyName("titulo")]
     public string? Titulo { get => titulo; set => titulo = value; }
+    [JsonPropertyName("estado")]
     public bool Estado { get => estado; set => estado = value; }
 }
