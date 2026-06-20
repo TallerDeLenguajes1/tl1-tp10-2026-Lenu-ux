1f0e1efa47465c471614faece0269bd2d333da97
Lenu-ux <lenis99lg@gmail.com>
2026-06-20 17:01:28 -0300

    modificado y terminado

--- MiWebApi/webServices.cs
+++ MiWebApi/webServices.cs
@@ -1,18 +1,28 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
 using System.Text.Json;
+using System.Threading.Tasks;
 using EspacioWeb;
 
 public static class WebServices
 {
     private static readonly HttpClient client=new HttpClient();
-    public static async Task<List<Webs>> GetWebs()
+    public static async Task<List<Web>> GetWebs()
     {
         var url= "http://universities.hipolabs.com/search?name=middle";
 
         HttpResponseMessage response = await client.GetAsync(url);
         response.EnsureSuccessStatusCode();
         string body= await response.Content.ReadAsStringAsync();
-        List <Webs>? listaweb=JsonSerializer.Deserialize<List<Webs>>(body);
+        List <Web>? listaweb=JsonSerializer.Deserialize<List<Web>>(body);
 
         return listaweb;
+    }
+
+    public static void generarArchivo(List<Web> webs)
+    {
+        string json= JsonSerializer.Serialize(webs);
+        File.WriteAllText("webs.json",json);
     }
 }
