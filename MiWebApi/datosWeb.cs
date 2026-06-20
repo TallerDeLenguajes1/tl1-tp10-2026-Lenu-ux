cb5ac8b6a00641a53ca81e8874f3ad930689ccb0
Lenu-ux <lenis99lg@gmail.com>
2026-06-20 15:43:04 -0300

    creacion de archivos

---
+++ MiWebApi/datosWeb.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Serialization;
+namespace EspacioWeb;
+// Root myDeserializedClass = JsonSerializer.Deserialize<List<Root>>(myJsonResponse);
+    public class Root
+    {
+        [JsonPropertyName("web_pages")]
+        public List<string> web_pages { get; set; }
+
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+        [JsonPropertyName("country")]
+        public string country { get; set; }
+
+        [JsonPropertyName("alpha_two_code")]
+        public string alpha_two_code { get; set; }
+
+        [JsonPropertyName("domains")]
+        public List<string> domains { get; set; }
+
+        [JsonPropertyName("state-province")]
+        public string stateprovince { get; set; }
+    }
+
+
