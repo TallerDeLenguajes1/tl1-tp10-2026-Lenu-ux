92343bd7749f7744ff9354729ade35d3770d1185
Lenu-ux <lenis99lg@gmail.com>
2026-06-20 11:32:32 -0300

    Creacion de carpeta ejercicio2

---
+++ Usuarios/usuario.cs
@@ -0,0 +1,59 @@
+using System.Text.Json.Serialization;
+
+namespace EspacioUsuario;
+public class Direccion
+{
+    [JsonPropertyName("street")]
+    public string Street { get; set; }
+    [JsonPropertyName("suite")]
+    public string Suite { get; set; }
+    [JsonPropertyName("city")]
+    public string City { get; set; }
+    [JsonPropertyName("zipcode")]
+    public string Zipcode { get; set; }
+    [JsonPropertyName("geo")]
+    public Geo Geo { get; set; }    
+}
+
+public class Company
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("catchPhrase")]
+    public string CatchPhrase { get; set; }
+    [JsonPropertyName("bs")]
+    public string Bs { get; set; }
+}
+
+public class Geo
+{
+    [JsonPropertyName("lat")]
+    public string lat{get; set; }
+    [JsonPropertyName("lng")]
+    public string lng{get; set; }
+}
+
+public class usuario
+{
+    [JsonPropertyName("id")]
+    public int id {get; set; }
+    [JsonPropertyName("name")]
+    public string name {get; set; }
+    [JsonPropertyName("usename")]
+    public string username{get; set; }
+    [JsonPropertyName("email")]
+    public string email{get; set; }
+
+    [JsonPropertyName("address")]
+    public Direccion addres {get; set;}
+
+    [JsonPropertyName("phone")]
+    public string celular {get; set; }
+
+    [JsonPropertyName("website")]
+    public string website {get; set; }
+
+    [JsonPropertyName("company")]
+    public Company compania {get; set; }
+    
+}
