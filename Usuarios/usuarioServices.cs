a93ce88db8ad17e5a074d2eabee79c8f39a10498
Lenu-ux <lenis99lg@gmail.com>
2026-06-20 11:53:56 -0300

    creacion de archivos de usuario servicios con serializacin y deserealizacion

---
+++ Usuarios/usuarioServices.cs
@@ -0,0 +1,28 @@
+using System.Text.Json;
+using espacioUsuario;
+
+public static class UsuarioServices
+{
+    private static readonly HttpClient client= new HttpClient();
+
+    public static async Task<List<Usuario>> GetUsuarios()
+    {
+        var url = "https://jsonplaceholder.typicode.com/users";
+        HttpResponseMessage response= await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        string body=await response.Content.ReadAsStringAsync();
+        List<Usuario>? listaUsuario=JsonSerializer.Deserialize<List<Usuario>>(body);
+
+        return listaUsuario;
+
+    }
+    
+    //serialisar
+public static void generarArchivo(List<Usuario> usuarios)
+{
+    string json= JsonSerializer.Serialize(usuarios);
+    File.WriteAllText("usuarios.json",json);
+}
+}
+
