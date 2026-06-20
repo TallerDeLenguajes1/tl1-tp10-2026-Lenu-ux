b4af0d28c91765b6008feb12d2ea14ae7bcd0af1
Lenu-ux <lenis99lg@gmail.com>
2026-06-20 15:01:24 -0300

    mostrar los 5 primeros usuarios

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Linq;
+using EspacioUsuario;
+
+List <Usuario> usuarios;
+usuarios= await UsuarioServices.GetUsuarios();
+
+foreach (var item in usuarios.Take(5))
+{
+    Console.WriteLine($"Id: {item.id}");
+    Console.WriteLine($"Nombre:{item.name}");
+    Console.WriteLine($"Email:{item.email}");
+    Console.WriteLine($"Direccion:{item.addres.Street},{item.addres.City}");
+    Console.WriteLine($"Telefono:{item.celular}");
+    Console.WriteLine($"WebSite:{item.website}");
+    Console.WriteLine($"Compania: {item.compania.Name}");
+    Console.WriteLine(new string('-',40));
+
+}
+UsuarioServices.generarArchivo(usuarios);
